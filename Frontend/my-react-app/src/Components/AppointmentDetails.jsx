5eae6febd435c1e261b02dec138eece963645ec1
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-04-25 04:14:26 +0530

    Resonsive design update

--- Frontend/my-react-app/src/Components/AppointmentDetails.jsx
+++ Frontend/my-react-app/src/Components/AppointmentDetails.jsx
@@ -4,7 +4,7 @@ const AppointmentDetails = ({appDetails, isOpen, onClose}) => {
     if(!isOpen) return null
   return (
     <div className='fixed inset-0 flex justify-center items-center bg-black bg-opacity-50'>
-        <div className='bg-white rounded-lg relative z-50 space-y-8 w-2/5 h-2/5 overflow-auto'>
+        <div className='bg-white rounded-lg relative z-50 space-y-8 md:w-2/5 w-2/3 h-2/5 overflow-auto'>
             <div className='p-2 rounded-lg'>
               <h2 className='text-2xl flex justify-center p-2 font-bold'>Appointment Details</h2>
               <h2 className='text-lg mx-4 my-3'><span className='font-bold'>Patient Name: </span>{appDetails.patient_name}</h2>
