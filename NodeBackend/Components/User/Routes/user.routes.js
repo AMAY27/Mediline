e920d9114e58e92f131faedaa615f65789be0975
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2025-07-03 21:18:04 +0530

    feature/User service addition to register new users

--- NodeBackend/Components/User/Routes/user.routes.js
+++ NodeBackend/Components/User/Routes/user.routes.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router();
 const userController = require('../Controllers/user.controller.js')
 const auth = require('../../../middlewares/jwtMiddleware.js')
+const validate = require('../middlewares/userValidationMiddleware.js')
+const validateUser  = require('../validation/userValidation.js')
 
 // router.post('/signup', userController.registerUser);
 // router.post('/login', userController.loginUser);
+router.post('/signup', validate(validateUser), userController.registerUser);
 router.post('/uploadReport', auth, userController.uploadReport);
 router.get('/getReports', auth, userController.getReportsForUser);
 
